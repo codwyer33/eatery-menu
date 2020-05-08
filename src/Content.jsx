83b9eb0d3305e7d99e4f3c254d48e867974c5a25
Clare O'Dwyer <codwyer23@23mbair390.local>
2020-05-08 14:01:01 -0400

    skeleton setup

---
+++ src/Content.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Content(){
+    return(
+    <div id="content">
+        <div id="left-arrow">
+
+        </div>
+        <div id="main-block">
+            <div id="block-top">
+                <h1>Datesdlfjlsdjf</h1>
+            </div>
+            <div id="block-mid-right">
+                <h2>ladjsf</h2>
+                <p>alsjfalksdjflkasdjfklasdjfkljdsakljfd</p>
+            </div>
+            <div id="block-mid-left">
+            <h2>ladjsf</h2>
+                <p>alsjfalksdjflkasdjfklasdjfkljdsakljfd</p>
+            </div>
+
+        </div>
+        <div id="right-arrow">
+
+        </div>      
+    </div>
+    );
+}
+
+export default Content;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-// import App from './App';
+import App from './App';
 
 ReactDOM.render(
-  <h1>this is clare</h1>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
