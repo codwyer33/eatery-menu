b50246ddb20eceab7a42d60927426383b4334a30
Clare O'Dwyer <codwyer23@23mbair390.local>
2020-05-08 14:27:43 -0400

    first round of styling

---
+++ src/Components/Content.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Content(){
+    return(
+    <div id="content">
+        <div id="intro">
+            <br></br>
+            <hr></hr>
+                <h1>You are cordially invited to Mommy's seaside Mother's Day Dinner!</h1>
+
+            <hr></hr>
+            <br></br>
+        </div>
+
+        <div id="main-block">
+            <div id="block-top">
+                <hr></hr>
+                <h1>Mommy's 2020 Mother's Day Dinner</h1>
+                <hr></hr>
+
+           </div>
+            <div id="block-a-right">
+                <h2>Appetizers</h2>
+                <p>alsjfalksdjflkasdjfklasdjfkljdsakljfd</p>
+            </div>
+            <div id="block-a-left">
+            <h2></h2>
+                <p>alsjfalksdjflkasdjfklasdjfkljdsakljfd</p>
+            </div>
+
+        </div>
+        
+    </div>
+    );
+}
+
+export default Content;
