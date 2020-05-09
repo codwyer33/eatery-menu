5e092927da84fd953092d78ca98bcdf225b4ed89
oodwyer <orodwyer@gmail.com>
2020-05-09 11:09:09 -0400

    email functionality

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -42,17 +42,137 @@ const desserts=[
 
 
 function Content(){
-    const [numDishes, setNumDishes] = useState({app: 2, sides: 2, entree: 1, dess: 2, drinks: 1});
-    const [order, setOrder] = useState({appetizers: ["test app"], sides: ["test side"], entrees: [], drinks: [], desserts: []});
+    const [numDishes, setNumDishes] = useState({app: 2, sides: 2, entree: 1, dess: 2, drinks: 2});
+    const [order, setOrder] = useState({appetizers: [], sides: [], entrees: [], drinks: [], desserts: []});
+    const [comment, setComment] = useState('');
+    const [email, setEmail] = useState('orodwyer@gmail.com');
+    const [name, setName] = useState('');
+    const [time, setTime] = useState('');
+
+    // var email = "orodwyer@gmail.com";
+    // var name = '';
+    // var time = '';
+
+    function getEmail(e) {
+        // email = e;
+        setEmail(e);
+        console.log(email);
+    }
+
+    function getName(n) {
+        // name = n; 
+        setName(n);
+        console.log(name);
+    }
+
+    function getTime(t) {
+        // time = t;
+        setTime(t);
+        console.log(time);
+    }
+
+    function changeComment(e) {
+        const value = e.target.value;
+        setComment(value);
+        // console.log(comment);
+        console.log(email);
+    }
 
     function sendEmail(e) {
+        setComment(comment);
+        var templateParams = {
+            to_email: email,
+            from_name: 'The Exquisite Eatery Team',
+            to_name: name,
+            message_html: '<h1>We are confirming your reservation on Sunday, May 10 at ' + time + '.</h1><br/>' + 
+            'Your appetizers: ' + order.appetizers + '<br/>' + 
+            'Your sides: ' + order.sides + '<br/>' + 
+            'Your entree: ' + order.entrees + '<br/>' + 
+            'Your desserts: ' + order.desserts + '<br/>' + 
+            'Your drink: ' + order.drinks + '<br/>' + 
+            'Additional Comments: ' + comment + '<br/>' + 
+            'We look forward to seeing you!'
+        };
+
         console.log("send email");
-        emailjs.sendForm('gmail', 'template_4XwuT01L', order, 'YOUR_USER_IDuser_NNbEaAW2hSuNXXPbx16QM')
+        emailjs.send('gmail', 'template_4XwuT01L', templateParams, 'user_NNbEaAW2hSuNXXPbx16QM')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
+    }
+
+    function addItem(category, item) {
+        switch (category) {
+            case 'Appetizers': 
+            setOrder(prevValue => { return {...prevValue, 
+                appetizers: [...order.appetizers, item]
+            }});
+            break;
+            case 'Sides': 
+            setOrder(prevValue => { return {...prevValue, 
+                sides: [...order.sides, item]
+            }});
+            break;
+            case 'Entrees': 
+            setOrder(prevValue => { return {...prevValue, 
+                entrees: [...order.entrees, item]
+            }});
+            break;
+            case 'Desserts': 
+            setOrder(prevValue => { return {...prevValue, 
+                desserts: [...order.desserts, item]
+            }});
+            break;
+            case 'Drinks': 
+            setOrder(prevValue => { return {...prevValue, 
+                drinks: [...order.drinks, item]
+            }});
+            break;
+        }
+        console.log(order);
+    }
+
+    function deleteItem(category, item) {
+        switch (category) {
+            case 'Appetizers': 
+            setOrder(prevValue => { return {...prevValue, 
+                appetizers: order.appetizers.filter(el => {
+                    return el !== item
+                })
+            }});
+            break;
+            case 'Sides': 
+            setOrder(prevValue => { return {...prevValue, 
+                sides: order.sides.filter(el => {
+                    return el !== item
+                })
+            }});
+            break;
+            case 'Entrees': 
+            setOrder(prevValue => { return {...prevValue, 
+                entrees: order.entrees.filter(el => {
+                    return el !== item
+                })
+            }});
+            break;
+            case 'Desserts': 
+            setOrder(prevValue => { return {...prevValue, 
+                desserts: order.desserts.filter(el => {
+                    return el !== item
+                })
+            }});
+            break;
+            case 'Drinks': 
+            setOrder(prevValue => { return {...prevValue, 
+                drinks: order.drinks.filter(el => {
+                    return el !== item
+                })
+            }});
+            break;
+        }
+        console.log(order);
     }
 
     return(
@@ -68,7 +188,7 @@ function Content(){
         </div>
 
         <div id="schedule-block">
-            <ScheduleForm />
+            <ScheduleForm getEmail={getEmail} getName={getName} getTime={getTime}/>
         </div>
 
         <div id="main-block">
@@ -79,19 +199,19 @@ function Content(){
 
            </div>
 
-            <MenuSec title="Appetizers" options={appetizers}/>
-            <MenuSec title="Sides" options={sides}/>
-            <MenuSec title="Entrees" options={entrees}/>
-            <MenuSec title="Desserts" options={desserts}/>
-            <MenuSec title="Drinks" options={drinks}/>
+            <MenuSec title="Appetizers" options={appetizers} addItem={addItem} deleteItem={deleteItem}/>
+            <MenuSec title="Sides" options={sides} addItem={addItem} deleteItem={deleteItem}/>
+            <MenuSec title="Entrees" options={entrees} addItem={addItem} deleteItem={deleteItem}/>
+            <MenuSec title="Desserts" options={desserts} addItem={addItem} deleteItem={deleteItem}/>
+            <MenuSec title="Drinks" options={drinks} addItem={addItem} deleteItem={deleteItem}/>
 
             <div id="submit-section">
                 <br/>
                 <div id="submit-left-align">
                     <label><h2>Please leave any additional comments or dietary restrictions below:</h2></label>
-                    <textarea  type="text" placeholder="Any additional comments" rows="10" cols="110"></textarea><br/>
+                    <textarea  onChange={changeComment} type="text" placeholder="Any additional comments" rows="10" cols="110" value={comment}></textarea><br/>
                 </div>
-                <button id="submit-btn" onClick={sendEmail} type="submit">Submit my order!</button>
+                <button id="submit-btn" onClick={sendEmail}>Submit my order!</button>
                 <br/><br/>
             </div>
         </div>
--- src/Components/ScheduleForm.jsx
+++ src/Components/ScheduleForm.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
-function ScheduleForm() {
+function ScheduleForm(props) {
   const [state, setName] = useState({name: "", set: false});
   const [timeState, setTime] = useState({time: "", timeSet: false});
   const [emailState, setEmail] = useState({email: "", emailSet: false});
+
+  // var form = document.getElementById("schedule-form");
+
+  // document.body.appendChild(form);
 
   function changeName(event) {
     const val = event.target.value;
@@ -11,9 +15,11 @@ function ScheduleForm() {
   }
 
   function changeView(event) {
+    event.preventDefault();
     setName(prevValue => {
       return {...prevValue, set: true}
-    })
+    });
+    props.getName(state.name);
   }
 
   function changeTime(event) {
@@ -21,13 +27,18 @@ function ScheduleForm() {
     setTime({time: value, set:false});
   }
 
-  function changeTimeView() {
+  function changeTimeView(event) {
+    event.preventDefault();
+
     setTime(prevValue => {
       return {...prevValue, timeSet: true}
     });
+    props.getTime(timeState.time);
   }
 
   function changeEmail(event) {
+    event.preventDefault();
+
     if (event.target.name === "email") {
       const val = event.target.value;
       setEmail({email: val, setEmail: false});
@@ -35,23 +46,25 @@ function ScheduleForm() {
       setEmail(prevValue => {
         return {...prevValue, emailSet: true}
       });
+      props.getEmail(emailState.email);
     }
     
   }
 
   return <div id="schedule-form">
-    {(!state.set) ? 
-    <form>
+    
+    <form id="form1" onSubmit={changeView}>
+    {(!state.set) ? <div>
       <label><h2>Please Enter Your Name:</h2></label>
       <input onChange={changeName} type="text" placeholder="Your Name" value={state.name}></input>
-      <button onClick={changeView} type="submit">Ready to go!</button>
-    </form> :
-    <h2>Welcome, {state.name}!</h2>}
+      <button  type="submit">Ready to go!</button></div> : <h2>Welcome, {state.name}!</h2>}
+    </form> 
       <br/><br/>
+
+    <form className="form">
     {(!timeState.timeSet) ? 
     <div>
     <h2>Select a time to dine on Sunday, May 10:</h2>
-    <form>
       <input onClick={changeTime} type="radio"  id="5:00pm" name="time" value="5:00pm"></input>
       <label for="5:00pm">5:00pm </label>
       <input onClick={changeTime} type="radio"  id="5:30pm" name="time" value="5:30pm"></input>
@@ -69,19 +82,21 @@ function ScheduleForm() {
       <label for="8:00pm">8:00pm </label>
 
       <input onClick={changeTimeView} className="btn" type="submit" value="Submit"></input>
-  </form></div> : 
-    <h3>Your reservation is set for {timeState.time} on  Sunday, May 10!</h3>}
+  </div> : 
+    <h3>Your reservation is set for {timeState.time} on  Sunday, May 10!</h3> }
+    </form>
 
   <br/><br/>
 
-  {!emailState.emailSet ? 
-  <form>
+  
+  <form className="form">
+  {!emailState.emailSet ? <div>
   <label><h2>If you would like to receive an email confirmation for your reservation and order, please enter your email:</h2></label>
       <input onChange={changeEmail} name="email" type="text" placeholder="Email" value={emailState.email}></input>
-      <button onClick={changeEmail} type="submit">Send me a confirmation!</button>
-  </form> :
+      <button onClick={changeEmail} type="submit">Send me a confirmation!</button></div>
+ :
   <h3>We'll shoot you a confirmation email at {emailState.email}!</h3>
-  }
+  }  </form>
 
   </div>
 }
